9b5b5285bb03d610b92bbaee5bc1e12b8c4114f8
Antje Barth <23338582+antje@users.noreply.github.com>
2020-02-02 13:51:19 +0100

    Update 04_analyze_items_with_short_reviews.sql
    corrected 'greater than 10' to 'less than 10' review_body length

--- sql/04_analyze_items_with_short_reviews.sql
+++ sql/04_analyze_items_with_short_reviews.sql
@@ -1,4 +1,4 @@
 SELECT *
 FROM amazon_reviews_parquet
 WHERE product_category = 'Digital_Video_Download'
-        AND LENGTH(review_body) > 10 LIMIT 20;
+        AND LENGTH(review_body) < 10 LIMIT 20;
